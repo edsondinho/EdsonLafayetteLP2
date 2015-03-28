c03b6d702b0a60ce8650df0c135d353b3f31013d
Edson Lafayette <edsonlsg@ig.com.br>
2015-03-28 17:58:36 -0300

    Exercício da aula do dia 24/03/2015

---
+++ Exercícios/aula_24032015/aula_24032015/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_24032015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            double num = 0;
+            double soma = 0;
+            double menor = 0;
+            double maior = 0;
+
+            Console.WriteLine("Digite um número.");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite {0} número(s).", n);
+
+            for (int i = 0; i < n; i++)
+            {
+                num = double.Parse(Console.ReadLine());
+                soma = soma + num;
+                
+                if (num > maior || i == 0)
+                {
+                    maior = num;
+                }
+
+                if (num < menor || i == 0) 
+                {
+                    menor = num;
+                }
+
+            }
+
+            Console.WriteLine("A soma dos números é {0}.", soma);
+            Console.WriteLine("O maior número é {0}.", maior);
+            Console.WriteLine("O menor número é {0}.", menor);
+        }
+    }
+}
