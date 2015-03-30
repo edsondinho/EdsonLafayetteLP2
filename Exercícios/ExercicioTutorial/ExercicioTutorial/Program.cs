6efbc35a7543b4f4ba5c3d12d02abc73386ad85c
Edson Lafayette <edsonlsg@ig.com.br>
2015-03-30 19:14:22 -0300

    Exercício do final do tutorial.

---
+++ Exercícios/ExercicioTutorial/ExercicioTutorial/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "";
+            //??????????????????????????????
+
+            Console.WriteLine("Digite uma frase.");
+            frase = Console.Read().ToString();
+
+            //??????????????????????????????
+
+            //if (??? > 1)
+            //{
+            //    Console.WriteLine("A frase possui {0} vogais.", ???);
+            //}
+
+            //else if (??? == 1)
+            //{
+            //    Console.WriteLine("A frase possui apenas uma vogal.");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("A frase não possui nenhuma vogal.");
+            //}
+
+        }
+    }
+}
