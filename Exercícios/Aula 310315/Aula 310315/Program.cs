ed797568101bdaedd7050daf3991f612ea2c414d
Edson Lafayette <edsonlsg@ig.com.br>
2015-03-31 18:14:06 -0300

    Adicionar aula 31/03/2015

---
+++ Exercícios/Aula 310315/Aula 310315/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_310315
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Código feito por André e Edson
+
+            int x;
+            int y;
+            int result = 1;
+
+            Console.Write("Digite seu expoente: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Digite o número da potêntica: ");
+            y = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < y; i++)
+            {
+                result *= x;
+            }
+
+            Console.WriteLine("O resultado é {0}.", result);
+
+        }
+    }
+}
