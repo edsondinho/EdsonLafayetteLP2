3dfac464234bdc7db5c57532f4527f035f203a1d
Edson Lafayette <edsonlsg@ig.com.br>
2015-04-04 15:43:54 -0300

    Adicionar Exercício 1 da Lista 1.

---
+++ Listas/Lista1/Lista1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FatorialDeN());
+        }
+
+        static string FatorialDeN() 
+        {
+            int N = -1;
+            int fatorial = 1;
+
+            while (N < 0)
+            {
+                Console.WriteLine("Escreva um número natural para calcular o seu fatorial.");
+                    N = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                fatorial *= (N - i); 
+            }
+
+            return N + "! " + "= " + fatorial;
+
+        }
+
+
+    }
+}
