f94a77b74c006ae85f625044d153f880031ad283
Edson Lafayette <edsonlsg@ig.com.br>
2015-04-28 15:58:25 -0300

    Adicionar exercício da aula do dia 28/04/2015

---
+++ Exercícios/aula__28042015/aula__28042015/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula__28042015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            n = int.Parse(Console.ReadLine());
+
+            int[] nums = new int[n];
+
+            int auxiliar = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int I = 0; I < n - 1; I++)
+                {
+                    {
+                        if (nums[I] > nums [I + 1])
+                        {
+                            auxiliar = nums[I];
+                            nums[I] = nums[I + 1];
+                            nums[I + 1] = auxiliar;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", nums[i]);
+            }
+
+        }
+    }
+}
