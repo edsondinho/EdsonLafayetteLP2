0c73a882bce43dff7ee1a5ae6fe00f11aa4686d4
Edson <dsonlsg@ig.com.br>
2015-09-15 17:04:39 -0300

    Adicionar Exercício 12 (versão sem herança)

---
+++ Exercícios/3º Certificação/Exercicio 12 (Sem Herança)/aula15092015/Gerente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula15092015
+{
+    class Gerente
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+        public double Salario { get; set; }
+        public string Matricula { get; set; }
+
+
+        public string AreaGenciada { get; set; }
+        public double Adicional { get; set; }
+        public double SalarioTotal()
+        {
+            return Salario + Adicional;
+        }
+    }
+}
---
+++ Exercícios/3º Certificação/Exercicio 12 (Sem Herança)/aula15092015/Trabalhador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula15092015
+{
+    class Trabalhador
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+        public double Salario { get; set; }
+        public string Matricula { get; set; }
+    }
+}
