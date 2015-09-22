54e15828707e203f67ddad90b2d0e78751cab4e8
Edson <edsonlsg@ig.com.br>
2015-09-22 17:56:30 -0300

    Adicionar exercício da aula do dia 22/09/2015

---
+++ Aulas/aula22092015/aula22092015/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace aula22092015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strConn = "Server=localhost;Database=test;Uid=root;Pwd=";
+
+            MySqlConnection conn = new MySqlConnection(strConn);
+
+            MySqlCommand command = new MySqlCommand();
+
+            string nome = "";
+            Console.Write("Nome: ");
+            nome = Console.ReadLine();
+
+            command.CommandText = "Insert Into Pessoa (Nome) Values ('" + nome + "')";
+
+            command.Connection = conn;
+
+            conn.Open();
+
+            command.ExecuteNonQuery();
+
+            conn.Close();
+
+        }
+    }
+}
