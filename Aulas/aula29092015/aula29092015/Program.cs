40a93ce0e279980855aed3bf8c9290d819dee667
Edson <dsonlsg@ig.com.br>
2015-09-29 18:00:35 -0300

    Adicionar Exercício da aula do dia 29/09/2015

---
+++ Aulas/aula29092015/aula29092015/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+
+namespace aula29092015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int escolha = int.MinValue;
+
+            while(escolha != 0)
+            {
+                escolha = int.Parse(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case 1:
+                        Consulta();
+                        break;
+
+                    case 2:
+                        Inserir();
+                        break;
+
+                    case 3:
+                        Remover();
+                        break;
+
+                    case 4:
+                        Atualizar();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void Consulta()
+        {
+            Console.WriteLine("Método de Consulta");
+            return;
+        }
+        public static void Inserir()
+        {
+            Console.WriteLine("Método de Inserir");
+            return;
+        }
+        public static void Remover()
+        {
+            Console.WriteLine("Método de Remover");
+            return;
+        }
+        public static void Atualizar()
+        {
+            Console.WriteLine("Método de Atualizar");
+            return;
+        }
+
+        public static void ConsultaBD()
+        {
+
+        }
+    }
+}
---
+++ Aulas/aula29092015/aula29092015/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("aula29092015")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("aula29092015")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0e749100-ec74-48a2-9fa7-bcc756b21ef0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
