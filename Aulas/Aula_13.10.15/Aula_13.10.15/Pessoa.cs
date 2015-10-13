725f89be153a4cf2a0dbfa39455cc42c51c242d8
Edson <edsonlsg@ig.com.br>
2015-10-13 18:01:59 -0300

    Adicionar Exercício da aula do dia 13/10/2015

---
+++ Aulas/Aula_13.10.15/Aula_13.10.15/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_13._10._15
+{
+    class Pessoa
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public double Peso { get; set; }
+
+        public double Altura { get; set; }
+
+        public string Telefone { get; set; }
+
+    
+    }
+}
---
+++ Aulas/Aula_13.10.15/Aula_13.10.15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Globalization;
+
+namespace Aula_13._10._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = new MySqlConnection("Server=Localhost;Database=test;Uid=Root;Pwd=");
+            while (true)
+            {
+                string escolha = Console.ReadLine();
+
+                if (escolha.ToLower() == "inserir")
+                {
+                    Pessoa pes = new Pessoa();
+                    pes.Nome = Console.ReadLine();
+                    pes.Sobrenome = Console.ReadLine();
+                    pes.Peso = double.Parse(Console.ReadLine());
+                    pes.Altura = double.Parse(Console.ReadLine());
+                    pes.Telefone = Console.ReadLine();
+                    cmd.CommandText = String.Format(new CultureInfo("en") ,"INSERT INTO PESSOA (NOME, SOBRENOME, PESO, ALTURA, TELEFONE) VALUES ('{0}','{1}',{2},{3},'{4}'",
+                        pes.Nome,pes.Sobrenome, pes.Peso, pes.Altura, pes.Telefone);
+                    cmd.Connection.Open();
+                    cmd.ExecuteNonQuery();
+                    cmd.Connection.Close();
+                }
+            }
+        }
+    }
+}
