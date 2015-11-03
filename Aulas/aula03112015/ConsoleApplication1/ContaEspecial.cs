156b40de4f3904ada07f167be3d9f4d16c523c93
Edson <edsonlsg@ig.com.br>
2015-11-03 17:52:00 -0300

    Adicionar exercício da aula do dia 03/11/2015

---
+++ Aulas/aula03112015/ConsoleApplication1/ContaEspecial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ContaEspecial : ContaBancaria 
+    {
+        public double Limite { get; set; }
+
+        public override string Sacar(double saque)
+        {
+            if (saque >= 0)
+            {
+                if (Saldo - saque >= -Limite)
+                {
+                    Saldo -= saque;
+                    return "O SAQUE DE R$" + saque + " FOI REALIZADO COM SUCESSO.";
+                }
+            }
+            return "SAQUE INVÁLIDO.";
+        }
+    }
+}
---
+++ Aulas/aula03112015/ConsoleApplication1/ContaPonpanca.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ContaPoupanca : ContaBancaria
+    {
+        public DateTime DiaDeRendimento { get; set; }
+
+        public void CalcularNovoSaldo(double TaxaRendimento)
+        {
+            Saldo += (Saldo * TaxaRendimento)/100;
+        }
+    }
+}
