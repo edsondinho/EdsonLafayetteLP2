7f23f201f88ac6e5b142637dfee0c344996bc5dc
Edson Lafayette <edsonlsg@ig.com.br>
2015-11-15 16:03:46 -0200

    Adicionar Exercicio 14 (Último exercício do trimestre)

---
+++ Exercícios/3º Certificação/Exercicio14/Exercicio14/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Exercicio14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = new MySqlConnection("Server=localhost;database=test;Uid=root;Pwd=");
+            MySqlDataReader read = new MySqlDataReader();
+            bool logado = false;
+
+            while (true)
+            {
+                if (logado == false)
+                {
+                    Console.WriteLine("Tela de Login");
+                    Console.Write("Digite seu Login: ");
+                    string login = Console.ReadLine();
+                    Console.Write("Digite sua senha:");
+                    string senha = Console.ReadLine();
+
+                    if (read.HasRows)
+                    {
+                        while (read.Read() && logado == false)
+                        {
+                            if (read.GetString(4) == login && read.GetString(5) == senha)
+                            {
+                                DateTime hj = DateTime.Today;
+                                cmd.CommandText = String.Format("INSERT INTO USUARIO (UltimoAcesso) VALUES ('{0}')", hj);
+                                cmd.Connection.Open();
+                                cmd.ExecuteNonQuery();
+                                cmd.Connection.Close();
+                                logado = true;
+                                Console.WriteLine("Login Realizado com sucesso.");
+                            }
+                        }
+
+                        if (logado == false)
+                            Console.WriteLine("Falha no login.");
+                    }
+
+                    else
+                        Console.WriteLine("Falha no login.");
+                }
+
+                else
+                {
+                    Console.WriteLine("Digite 'sair' para deslogar.");
+                    string sair = Console.ReadLine();
+
+                    if (sair.ToLower() == "sair")
+                    {
+                        logado = false;
+                        Console.WriteLine("Você foi deslogado com sucesso.");
+                    }
+                }
+            }
+
+        }
+    }
+}
